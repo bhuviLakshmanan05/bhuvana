b32da7a1fddf64aba7b339234661820694fe791e
bhuviLakshmanan05 <bhuvaneswaritnj96@gmail.com>
2018-03-06 22:37:33 +0530

    my prog1

--- pos.c
+++ pos.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-clrscr();
+#include <stdio.h>
+ 
+int main(void) {
+
+ 
 int n;
-printf("enter n");
+ 
 scanf("%d",&n);
 if(n>=1&&n<=100000)
 printf("\npositive");
-getch();
-return 0;
+ 
+	return 0;
 }
